2de95b7948352bd3fffeac4f3fbb038214ba86a3
Ryan Gallaway <rkgallaway@msn.com>
2019-09-18 11:50:22 -0700

    class 05 demo added

---
+++ curriculum/class-06/starter-code/js/add-content.js
@@ -0,0 +1,15 @@
+// var today = new Date();
+var hourNow = prompt("What's the hour?");
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>'+ greeting + '</h3>');
